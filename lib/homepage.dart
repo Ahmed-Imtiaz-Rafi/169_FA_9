8285a134d8265dee5b988755f6e47ec5d847f04f
Ahmed-Imtiaz-Rafi <irafiy07@gmail.com>
2026-04-19 23:41:43 +0600

    Revamp homepage to display digital ID card
    Updated the homepage layout to include a digital ID card with user details.

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,41 +7,59 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Homepage'),
+        title: Text('Digital ID Card'),
         backgroundColor: Colors.blueAccent,
         centerTitle: true,
       ),
-
-      //margin: EdgeInsets.only(top: 50),
-      body: Container(
-        margin: EdgeInsets.only(top: 50),
-        child: Column(
-          spacing: 50,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(width: 200, height: 100, color: Colors.amberAccent),
-                Container(width: 200, height: 100, color: Colors.amberAccent),
-                Container(width: 200, height: 100, color: Colors.amberAccent),
-              ],
-            ),
-            Center(
-              child: Container(
-                width: 900,
-                height: 100,
-                color: Colors.redAccent,
-              ),
-            ),
-            Row(
-              spacing: 100,
+      body: Center(
+        child: SizedBox(
+          width: 300,
+          height: 400,
+          child: Card(
+            child: Column(
+              spacing: 20,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(width: 150, height: 100, color: Colors.blueGrey),
-                Container(width: 150, height: 100, color: Colors.blueGrey),
+                CircleAvatar(
+                  backgroundImage: AssetImage(
+                    'assets/images/17dfa916f8e4ca133dbbf0b4fbf76c3c.jpg',
+                  ),
+                  maxRadius: 35,
+                ),
+                Text(
+                  'Ahmed Imtiaz Rafi\n'
+                  'Batch: 63rd, Department of CSE',
+                  textAlign: TextAlign.center,
+                ),
+                Divider(),
+                ListTile(
+                  subtitle: Column(
+                    spacing: 20,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.person),
+                          Text('ID: 0182410012101169'),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.email),
+                          Text('Email: irafiy07@gmail.com'),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.phone),
+                          Text('Phone: 0123791328'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
